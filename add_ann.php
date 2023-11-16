a683fe0882f312f8cfe46a34d13033c1be0c38ac
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-16 18:07:07 +0100

    Add a New Announcement page

---
+++ add_ann.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Add a New Announcement</title>
+</head>
+<body>
+<div class="max-w-md mx-auto mt-8">
+    <h1 class="text-3xl font-bold mb-4 text-center mb-[20px] text-blue-500 mb-10">Add a New Announcement</h1>
+
+    <form action="/add_ann_treatment.php" method="post" class="bg-white p-6 rounded shadow-md">
+        <div class="mb-4">
+            <label for="title" class="block text-gray-700 font-semibold mb-2">Title:</label>
+            <input type="text" id="title" name="title" required
+                   class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="mb-4">
+            <label for="category" class="block text-gray-700 font-semibold mb-2">Category:</label>
+            <input type="text" id="category" name="category" required
+                   class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="mb-4">
+            <label for="description" class="block text-gray-700 font-semibold mb-2">Description:</label>
+            <textarea id="description" name="description" rows="4" required
+                      class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
+        </div>
+        <div class="mb-4">
+            <label for="price" class="block text-gray-700 font-semibold mb-2">Price:</label>
+            <input type="number" id="price" name="price" required
+                   class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="text-center">
+            <input type="submit" value="Add" class="hover:opacity-80 font-bold transition-all bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
