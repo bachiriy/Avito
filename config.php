ffffb940056e3534b68fc43836d1e19f38f69bc4
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-16 18:05:28 +0100

    create announcement table in db

--- config.php
+++ config.php
@@ -9,5 +9,12 @@ $connect = mysqli_connect($hostname, $username, $password, $database);
 if($connect == false){
     die("Could not connect to Database !");
 }
+$announcement_table = "CREATE TABLE IF NOT EXISTS announcement(ID INT PRIMARY KEY AUTO_INCREMENT,  Title VARCHAR(100), Category VARCHAR(30), Description TEXT, Price INT)";
+
+$result = mysqli_query($connect, $announcement_table);
+
+if($result == false){
+    die("Could not Add Table !");
+}
 
 ?>
