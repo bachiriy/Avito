109e11c6dead174d05c723da92eaf1204276a13f
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-17 17:15:46 +0100

    Update edit_ann_traitment.php

--- edit_ann_traitment.php
+++ edit_ann_traitment.php
@@ -1,3 +1,17 @@
 <?php
 include 'config.php';
+
+if (isset($_POST['edit'])) {
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $category = $_POST['cat'];
+    $description = $_POST['desc'];
+    $price = $_POST['price'];
+
+    $result = $prepare->execute();
+
+    if ($result == true) {
+        header('location: index.php');
+    }
+
 }
