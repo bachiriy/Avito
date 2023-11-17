51be1992e126d9e072c2723b00d057f99b948086
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-17 10:23:51 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,18 @@
 <?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'avito_database';
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "avito_database";
 
 $connect = mysqli_connect($hostname, $username, $password, $database);
 
 if($connect == false){
     die("Could not connect to Database !");
 }
-$announcement_table = "CREATE TABLE IF NOT EXISTS announcement(ID INT PRIMARY KEY AUTO_INCREMENT,  Title VARCHAR(100), Category VARCHAR(30), Description TEXT, Price INT)";
 
-$result = mysqli_query($connect, $announcement_table);
+$sql = "CREATE TABLE IF NOT EXISTS announcement(ID INT PRIMARY KEY AUTO_INCREMENT,  Title VARCHAR(100), Category VARCHAR(30), Description TEXT, Price INT);";
+
+$result = mysqli_query($connect, $sql);
 
 if($result == false){
     die("Could not Add Table !");
