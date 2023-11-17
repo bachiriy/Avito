4ef38d4d2ee549c8747326426008cb09f8eff8b8
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-17 10:23:13 +0100

    update button

--- add_ann.php
+++ add_ann.php
@@ -10,7 +10,7 @@
 <div class="max-w-md mx-auto mt-8">
     <h1 class="text-3xl font-bold mb-4 text-center mb-[20px] text-blue-500 mb-10">Add a New Announcement</h1>
 
-    <form action="/add_ann_treatment.php" method="post" class="bg-white p-6 rounded shadow-md">
+    <form action="add_ann_treatment.php" method="post" class="bg-white p-6 rounded shadow-md">
         <div class="mb-4">
             <label for="title" class="block text-gray-700 font-semibold mb-2">Title:</label>
             <input type="text" id="title" name="title" required
@@ -32,7 +32,7 @@
                    class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
         </div>
         <div class="text-center">
-            <input type="submit" value="Add" class="hover:opacity-80 font-bold transition-all bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">
+            <button name="add" class="hover:opacity-80 font-bold transition-all bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer">ADD</button>
         </div>
     </form>
 </div>
