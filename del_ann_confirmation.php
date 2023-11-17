c1582d0f7a7a0bbb18ca2b9176233af60eeace26
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2023-11-17 19:52:36 +0100

    Update del_ann_confirmation.php

--- del_ann_confirmation.php
+++ del_ann_confirmation.php
@@ -10,11 +10,10 @@
     <p class="mb-4 text-lg">Are you sure you want to delete this announcement?</p>
 
     <form  action="del_ann_treatment.php" method="post"  class="flex justify-end space-x-4" id="yes-no-btns">
-
         <input type="hidden" value="<?php echo $_POST['id']; ?>" name="id">
         <button name="yes_btn" type="submit" class="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-700 transition-all">Yes</button>
-    </form>
     <a href="index.php" type="button" class="bg-gray-300 text-gray-800 py-2 px-4 rounded-md hover:bg-gray-400 transition-all">No</a>
+    </form>
 </div>
 </body>
 </html>
